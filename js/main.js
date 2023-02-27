3ca3c48bc63ddf4cdd2753ba630ead580d110c37
Herenyon <rikitur17@gmail.com>
2023-02-27 22:22:46 +0100

    seconda bozza js

--- js/main.js
+++ js/main.js
@@ -1,50 +1,57 @@
 'use script'
 
-const arrayImg = ['01.webp', '02.webp', '03.webp', '04.webp', '05.webp',];
-
+const arrayImg = ['01.webp', '02.webp', '03.webp', '04.webp', '05.webp'];
+console.log(arrayImg.length);
 let valoreBottoni = 0;
-
+console.log(valoreBottoni);
 let immagini = '';
 
-const incrementoBtn = document.getElementById(giacomo);
-const decrementoBtn = document.getElementById(btnGiu);
+const containerImg = document.querySelector('.containerImg');
+const incrementoBtn = document.getElementById("btnSu");
+const decrementoBtn = document.getElementById("btnGiu");
 
-console.log(giacomo);
-for (let i = 0; i <= arrayImg.length; i++) {
+
+for (let i = 0; i < arrayImg.length; i++) {
 
     immagini += `<div class="img"><img src="img/${arrayImg[i]}"></div>`;
-
-
+    
 
 }
+containerImg.innerHTML = containerImg.innerHTML + immagini;
+
+document.querySelector(".img").classList.add("show");
+const pictures = document.querySelectorAll('.img');
+console.log(pictures);
+console.log(valoreBottoni);
 
 incrementoBtn.addEventListener('click',
     function(){
         valoreBottoni++;
-
+   
         if (valoreBottoni === arrayImg.length){
             
             valoreBottoni = 0;
 
         }
 
-        console.log(valoreBottoni)
+    
     }
     
-    
+   
 )
 
 decrementoBtn.addEventListener('click',
     function(){
-        valoreBottoni = arrayImg.length;
-
+        valoreBottoni--;
+        pictures[valoreBottoni].classList.remove('show');
+        
         if (valoreBottoni === -1) { 
             
-            valoreBottoni = arrayImg.length 
+            valoreBottoni = arrayImg.length;
         
         }
 
-        console.log(valoreBottoni)
+        console.log(valoreBottoni);
         
 
     }
