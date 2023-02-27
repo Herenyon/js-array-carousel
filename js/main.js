4a48e3b503c49cc75ee01cd96c5add0b792b53be
Herenyon <rikitur17@gmail.com>
2023-02-27 22:57:34 +0100

    fine esercizio

--- js/main.js
+++ js/main.js
@@ -1,61 +1,51 @@
 'use script'
 
 const arrayImg = ['01.webp', '02.webp', '03.webp', '04.webp', '05.webp'];
-console.log(arrayImg.length);
-let valoreBottoni = 0;
-console.log(valoreBottoni);
+
 let immagini = '';
-
 const containerImg = document.querySelector('.containerImg');
-const incrementoBtn = document.getElementById("btnSu");
-const decrementoBtn = document.getElementById("btnGiu");
-
 
 for (let i = 0; i < arrayImg.length; i++) {
 
     immagini += `<div class="img"><img src="img/${arrayImg[i]}"></div>`;
-    
+
 
 }
 containerImg.innerHTML = containerImg.innerHTML + immagini;
-
 document.querySelector(".img").classList.add("show");
+
+const incrementoBtn = document.getElementById("btnSu");
+const decrementoBtn = document.getElementById("btnGiu");
+
+let valoreBottoni = 0;
 const pictures = document.querySelectorAll('.img');
-console.log(pictures);
+
 console.log(valoreBottoni);
 
 incrementoBtn.addEventListener('click',
-    function(){
-        valoreBottoni++;
-   
-        if (valoreBottoni === arrayImg.length){
-            
-            valoreBottoni = 0;
+    function () {
+
+        console.log(valoreBottoni)
+        if (valoreBottoni < arrayImg.length - 1) {
+
+
+            pictures[valoreBottoni].classList.remove('show');
+            valoreBottoni++;
+            pictures[valoreBottoni].classList.add('show');
 
         }
-
-    
     }
-    
-   
 )
 
 decrementoBtn.addEventListener('click',
-    function(){
-        valoreBottoni--;
-        pictures[valoreBottoni].classList.remove('show');
-        
-        if (valoreBottoni === -1) { 
-            
-            valoreBottoni = arrayImg.length;
-        
+    function () {
+
+        if (valoreBottoni > 0) {
+
+            pictures[valoreBottoni].classList.remove('show');
+            valoreBottoni--;
+            pictures[valoreBottoni].classList.add('show');
+
         }
-
-        console.log(valoreBottoni);
-        
-
     }
-    
-    
-    
 )
